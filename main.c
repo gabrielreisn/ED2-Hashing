cf60ae3f8be12bdbc54edd5db6dac30c08497422
gferreira <gabrielreisnf@gmail.com>
2017-06-22 13:27:31 -0300

    initial commit..

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: gferreira
+ *
+ * Created on June 22, 2017, 1:26 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
